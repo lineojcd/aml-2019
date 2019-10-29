8b1ae96e5e3b0d30e3b10fea5ae7af3f963451a2
Yongqi WANG <wangyq977@gmail.com>
2019-10-29 12:25:47 +0100

    add some cv result

--- helper/tuning.py
+++ helper/tuning.py
@@ -3,26 +3,22 @@ from time import time, strftime
 import joblib
 from sklearn.model_selection import cross_val_score, GridSearchCV, RandomizedSearchCV, ShuffleSplit
 from matplotlib import pyplot as plt
+import pprint
 from .visual import grid_search_plot, plot_grid_search_all
 
 
-def tune(pipeline, param_grid, X, y, save=True, scoring='r2'):
+def tune(pipeline, param_grid, X, y, save=True, scoring='r2', verbose=0):
     if save and not os.path.exists('log'):
         os.makedirs('log')
     timestr = strftime("%Y%m%d-%H%M%S")
     cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
     grid_search = GridSearchCV(pipeline, param_grid, scoring=scoring, cv=cv,
-                               n_jobs=-1)
-    t0 = time()
+                               n_jobs=-1, verbose=verbose)
     grid_search.fit(X, y)
 
-    print("done in {:.3f}s\n".format(time() - t0))
-    print("Best score: {:.3f} +/- {:.3f}".format(grid_search.best_score_,
-                                                 grid_search.cv_results_['std_test_score'][grid_search.best_index_]))
-    print("Best parameters set:")
-    best_parameters = grid_search.best_estimator_.get_params()
-    for param_name in sorted(param_grid.keys()):
-        print("\t%s: %r" % (param_name, best_parameters[param_name]))
+    for mean, std, param in zip(grid_search.cv_results_['mean_test_score'], grid_search.cv_results_['std_test_score'], grid_search.cv_results_['params']):
+        print("Score: {:.3f} +/- {:.3f}".format(mean, std))
+        pprint.pprint(param)
 
     if save:
         plot_grid_search_all(
--- task2/main.py
+++ task2/main.py
@@ -11,56 +11,48 @@ import sklearn
 import pandas as pd
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import LocalOutlierFactor, KNeighborsClassifier
-from sklearn.base import TransformerMixin
+from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.impute import SimpleImputer
 from sklearn.feature_selection import SelectKBest, f_regression, VarianceThreshold
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score, train_test_split, RandomizedSearchCV, GridSearchCV, ShuffleSplit
+from lightgbm import LGBMClassifier
 
-X = pd.read_csv("data/X_train.csv", index_col=0).values
-y = pd.read_csv('data/y_train.csv', index_col=0).values
+X = pd.read_csv("{}/data/X_train.csv".format(dir_cur), index_col=0).values
+y = pd.read_csv("{}/data/y_train.csv".format(dir_cur), index_col=0).values
 y = sklearn.utils.validation.column_or_1d(y)
+
+def outliers_removal(X, y):
+    outliers = LocalOutlierFactor().fit_predict(X, y)
+    return X[outliers == 1], y[outliers == 1]
+
+X, y = outliers_removal(X, y)
 
 pipeline = Pipeline([
     ('scaler', StandardScaler()),
     ('smote', ClusterCentroids()),
-    ('clf', SVC()),
+    ('clf', RandomForestClassifier(random_state=1)),
 ])
 
 param_grid = {
+    'clf__n_estimators': [50, 100, 200, 500],
 }
-
-param_rf = {
-    'n_estimators': np.linspace(10, 100, num=4, dtype=np.int),
-}
-
-param_svm = {
-    'C': [1.0, 2.0],
-}
-
-param_knn = {
-    'n_neighbors': [1, 5],
-}
-
 
 def main():
     pipeline.fit(X, y)
-    X_test = pd.read_csv('data/X_test.csv', index_col=0).values
+    X_test = pd.read_csv("{}/data/X_test.csv".format(dir_cur), index_col=0).values
     pred = pipeline.predict(X_test)
-    np.savetxt('submission.csv', np.dstack((np.arange(0, pred.size), pred))[0], '%0.1f,%0.1f',
+    np.savetxt("{}/submission.csv".format(dir_cur), np.dstack((np.arange(0, pred.size), pred))[0], '%0.1f,%0.1f',
                comments='', header="id,y")
-    print(np.unique(pred, return_counts=True))
 
 
 if __name__ == "__main__":
-    main()
-    cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-    cv_score = cross_val_score(
-        pipeline, X, y, cv=cv, scoring='balanced_accuracy')
-    print("{:.3f} +/- {:.3f}".format(np.mean(cv_score), np.std(cv_score)))
+    import warnings
+    warnings.filterwarnings("ignore")
+    print('X shape: {},\t y shape: {}'.format(X.shape, y.shape))
+    # main()
     # param_grid = {'clf__'+k: v for k, v in param_svm.items()}
-    # param_grid['smote'] = [SMOTE(), RandomOverSampler()]
-    # tune(pipeline, param_grid, X, y, save=1, scoring='balanced_accuracy')
+    tune(pipeline, param_grid, X, y, save=1, scoring='balanced_accuracy', verbose=0)
