cc6802bc74288d4ff351e10d617867ab899d90dd
Yongqi Wang <wangyong@eu-login-06-ng.euler.ethz.ch>
2019-11-09 16:17:45 +0100

    changed nn

--- task2/main.py
+++ task2/main.py
@@ -4,27 +4,28 @@ dir_cur = os.path.abspath(os.path.dirname(__file__))
 dir_par = os.path.dirname(dir_cur)
 sys.path.insert(0, dir_par)
 from helper.tuning import tune
+from helper.pipeline_tool import PrintXShape
 import warnings
 warnings.filterwarnings("ignore")
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # ignore tf warning
 
-from imblearn.under_sampling import ClusterCentroids
-from imblearn.over_sampling import RandomOverSampler, SMOTE
-from imblearn.pipeline import Pipeline
-import sklearn
 import pandas as pd
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
-from sklearn.svm import SVC
-from sklearn.neighbors import LocalOutlierFactor, KNeighborsClassifier
-from sklearn.base import TransformerMixin, BaseEstimator
-from sklearn.impute import SimpleImputer
+import sklearn
+import keras
+from keras.models import Model, Sequential
+from keras.layers import Dropout, Dense, Input, Reshape, Flatten, GlobalAveragePooling2D
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score, ShuffleSplit, KFold
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import SelectKBest, f_regression, VarianceThreshold
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import cross_val_score, train_test_split, RandomizedSearchCV, GridSearchCV, ShuffleSplit
-from lightgbm import LGBMClassifier
-from sklearn.neural_network import MLPClassifier
-# from sklearn.pipeline import Pipeline
+from sklearn.neighbors import LocalOutlierFactor
+from imblearn.under_sampling import ClusterCentroids
+from imblearn.over_sampling import RandomOverSampler, SMOTE
+from imblearn.combine import SMOTEENN, SMOTETomek
+from imblearn.pipeline import Pipeline
 
 
 def read_data():
@@ -36,30 +37,44 @@ def read_data():
     return X, y
 
 
-def outliers_removal(X, y):
-    outliers = LocalOutlierFactor().fit_predict(X, y)
-    return X[outliers == 1], y[outliers == 1]
+def outliers_removal(X, y, verbose=False):
+    outliers = LocalOutlierFactor(n_neighbors=40, contamination=0.08).fit_predict(X, y)
+    X_, y_ = X[outliers == 1], y[outliers == 1]
+    if not verbose:
+        print('Removal outliers...')
+        print('{} outliers removed\n'.format(X.shape[0] - X_.shape[0]))
+    return X_, y_
+
+
+def baseline_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(200, input_dim=1000, activation='relu', kernel_initializer='random_uniform'))
+    model.add(Dropout(0.3))
+    model.add(Dense(3, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adadelta(), metrics=['categorical_accuracy'])
+    return model
 
 
 pipeline = Pipeline([
+    ('sampler', ClusterCentroids()),
     ('scaler', StandardScaler()),
-    # ('var', VarianceThreshold(0.01)),
-    # ('kbest', SelectKBest(f_regression, k=700)),
-    ('cc', ClusterCentroids()),
-    ('clf', SVC(gamma='scale', class_weight='balanced', decision_function_shape='ovo',
-                kernel='rbf')),
+    # ('debug', PrintXShape('ClusterCentroids')),
+    ('nn', KerasClassifier(build_fn=baseline_model, epochs=100, batch_size=100, verbose=0)),
 ])
 
 param_grid = {
-    # 'clf__n_estimators': [50, 100, 200, 500],
-    'kbest__k': [200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    'nn__batch_size': [20, 100, 200]
 }
 
 
 def main():
+    print('Fitting...')
     pipeline.fit(X, y)
     X_test = pd.read_csv(
         "{}/data/X_test.csv".format(dir_cur), index_col=0).values
+    print('Predicting...')
     pred = pipeline.predict(X_test)
     np.savetxt("{}/submission.csv".format(dir_cur),
                np.dstack((np.arange(0, pred.size), pred))[0],
@@ -70,13 +85,7 @@ if __name__ == "__main__":
     import time
     t0 = time.time()
     X, y = read_data()
-    # X, y = outliers_removal(X, y)
+    X, y = outliers_removal(X, y, 1)
     # main()
-    cv = ShuffleSplit(n_splits=2, test_size=0.2, random_state=0)
-    cv_score = cross_val_score(
-        pipeline, X, y, scoring='balanced_accuracy', cv=cv)
-    # pipeline.set_params(clf__gamma=gamma)
+    tune(pipeline, param_grid, X, y, save=1, scoring='balanced_accuracy', verbose=1)
     print('Time : {:.3f} sec'.format(time.time() - t0))
-    print("Score: {:.3f} +/- {:.3f}".format(np.mean(cv_score), np.std(cv_score)))
-    # param_grid = {'clf__'+k: v for k, v in param_svm.items()}
-    # tune(pipeline, param_grid, X, y, save=1, scoring='balanced_accuracy', verbose=1)
